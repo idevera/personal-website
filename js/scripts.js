013e12aed7f8d0b60df992097f5ab6658d179667
Irene <irenneeyyy@gmail.com>
2017-05-26 17:05:12 -0700

    Add hover effects to contact footer

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
+//Title fades in on load screen
+$(window).on('load', function() {
+  $("div#header-container").fadeIn(2000).removeClass('hidden');  });
 
-  $(window).on('load', function() {
-    $("div#header-container").fadeIn(2000).removeClass('hidden');
+$(document).ready(function(){
+  //Contact at bottom mouseover effect
+  $("ul li a").mouseover(function () {
+      $("ul li a").not($(this)).addClass('hover');
   });
+  //Contact at bottome mouseout effect
+  $("ul li a").mouseout(function () {
+      $("ul li a").not($(this)).removeClass('hover');
+  });
+});
+
+  // var $animation_elements = $('.animation-element');
+  // var $window = $(window);
